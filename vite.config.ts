e83535c4fb3c12767a755261ef7659531e50cbea
jelliots2 <jelliot.stocker@gmail.com>
2024-10-13 13:35:06 -0400

    ugh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './dist', // You can modify this if deploying to a subpath
+  base: './', // You can modify this if deploying to a subpath
 });
